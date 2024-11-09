c45b75964a5d9d65fa9e91d94bd0c6a008461431
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2024-11-09 18:45:48 +0530

    more files converted

--- src/etl/transform/2.stage2/createPlayer_18.py
+++ src/etl/transform/2.stage2/createPlayer_18.py
@@ -1,10 +1,10 @@
 import duckdb
 connection = duckdb.connect(":memory:")
 
-outputParquetPath = 'D:/VS Code/FullStackDashboardsProject/FullStackDashboardProject-3/FDP3Backend-ETL/data/transformed/2.stage2/Players_Data.parquet'
+outputParquetPath = 'D:/VS Code/FullStackDashboardsProject/FullStackDashboardProject-3/FDP3Backend-ETL/data/transformed/2.stage2/Players_Data_18.parquet'
 
 connection.execute(f"""
-                   CREATE TABLE Players_Data AS SELECT * FROM read_parquet('data/transformed/1.stage1/Fifa_17.parquet')
+                   CREATE TABLE Players_Data AS SELECT * FROM read_parquet('data/transformed/1.stage1/Fifa_18.parquet')
                    """)
 query = f"""SELECT pd.ID, pd.Name, pd.Age, pd.Photo, pd.Nationality, pd.Flag, pd.Club, pd."Jersey Number", pd.Joined, pd.SprintSpeed, pd.Agility, pd.Balance, pd.ShotPower, pd.Jumping, pd.Stamina, pd.Volleys, pd.Height, pd.Weight FROM Players_Data pd"""
 connection.execute(
@@ -13,4 +13,4 @@ connection.execute(
     """
 )
 
-print ("Fifa Stage 2 Done !! 🎉")
+print ("Fifa Stage 2 Done !! 💖")
