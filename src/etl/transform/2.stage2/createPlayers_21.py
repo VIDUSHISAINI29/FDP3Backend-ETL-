346f7ece309291cd009f648e9e78dcc4dda9c5cd
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2024-11-25 20:51:17 +0530

    more files prepared

---
+++ src/etl/transform/2.stage2/createPlayers_21.py
@@ -0,0 +1,16 @@
+import duckdb
+connection = duckdb.connect(":memory:")
+
+outputParquetPath = 'D:/VS Code/FullStackDashboardsProject/FullStackDashboardProject-3/FDP3Backend-ETL/data/transformed/2.stage2/Players_Data_21.parquet'
+
+connection.execute(f"""
+                   CREATE TABLE Players_Data AS SELECT * FROM read_parquet('data/transformed/1.stage1/Fifa_21.parquet')
+                   """)
+query = f"""SELECT pd.ID, pd.Name, pd.Age, pd.Photo, pd.Nationality, pd.Flag, pd.Club, pd."Jersey Number", pd.Joined, pd.SprintSpeed, pd.Agility, pd.Balance, pd.ShotPower, pd.Jumping, pd.Stamina, pd.Volleys, pd.Height, pd.Weight FROM Players_Data pd"""
+connection.execute(
+    f"""
+    COPY ({query}) TO '{outputParquetPath}' (FORMAT PARQUET)
+    """
+)
+
+print ("Fifa_21 Stage 2 Done !! 💖")
